7aa9687b6041f88752f57cf5de77bb0bd781756a
mcm-source <martinmcm@hotmail.es>
2020-08-24 21:08:07 +0200

    variables

---
+++ index.js
@@ -0,0 +1,7 @@
+var name;
+var apellido = 'Castro';
+name = 'Pepe Do Monte';
+var edad = 28;
+console.log('Hola mundo ' + name + ' ' + apellido);
+console.log('Tengo ' + edad + ' años');
+var peso = 75;
