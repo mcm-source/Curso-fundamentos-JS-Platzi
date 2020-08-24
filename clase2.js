837a3e9e34de106f46b9a1fb3e93447eac449f65
mcm-source <martinmcm@hotmail.es>
2020-08-24 21:22:01 +0200

    variables2

---
+++ clase2.js
@@ -0,0 +1,8 @@
+var nombre = 'Sacha';
+var apellido = 'Lifszyc';
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLowerCase();
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length;
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
+var str = nombre.substr(1, 2);
