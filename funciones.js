85645ddeb933de16d79fbf335ff0c362fa1b4adf
mcm-source <martinmcm@hotmail.es>
2020-08-24 21:51:40 +0200

    funciones

---
+++ funciones.js
@@ -0,0 +1,10 @@
+var nombre = 'Sacha';
+var edad = 28;
+
+function imprimirEdad(n, e) {
+  console.log(`${n} tiene ${e}`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Vicky', 28);
+imprimirEdad(28, 'Pepe');
