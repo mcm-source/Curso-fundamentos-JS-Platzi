2261450c040d9d5970daa45f526d0c10999df08d
mcm-source <martinmcm@hotmail.es>
2020-08-24 21:24:03 +0200

    reto variables

--- clase2.js
+++ clase2.js
@@ -6,3 +6,4 @@ var primeraLetraDelNombre = nombre.charAt(0);
 var cantidadDeLetrasDelNombre = nombre.length;
 var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
 var str = nombre.substr(1, 2);
+var ultimaLetra = nombre.charAt(nombre.length - 1);
