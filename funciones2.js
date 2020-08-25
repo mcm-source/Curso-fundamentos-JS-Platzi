740cece757c39cf14759fc629afdc74c54d75b88
mcm-source <martinmcm@hotmail.es>
2020-08-25 19:07:27 +0200

    objetos

--- funciones2.js
+++ funciones2.js
@@ -1,9 +1,26 @@
-var nombre = 'Sacha';
+var sacha = {
+  nombre: 'Sacha',
+  apellidos: 'Lifszyc',
+  edad: 28,
+};
 
-function imprimirNombreEnMayusculas(n) {
-  n = n.toUpperCase();
-  console.log(n);
+var dario = {
+  nombre: 'Darío',
+  apellido: 'Perez',
+  edad: 27,
+};
+
+var pepe = {
+  apellido: 'Perez',
+  edad: 27,
+};
+
+function imprimirNombreEnMayusculas({ nombre }) {
+  var nombre = nombre.toUpperCase();
+  console.log(nombre);
 }
 
-imprimirNombreEnMayusculas(nombre);
-console.log(nombre);
+imprimirNombreEnMayusculas(sacha);
+imprimirNombreEnMayusculas({ nombre: 'Sacha' });
+// imprimirNombreEnMayusculas({apellido:"Lifszyc"});
+console.log();
