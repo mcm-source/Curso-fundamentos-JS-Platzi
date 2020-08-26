cc04c7c3c12302723076da5c136e6d4cb1608c5f
mcm-source <martinmcm@hotmail.es>
2020-08-26 20:22:14 +0200

    Condicionales

--- condicionales.js
+++ condicionales.js
@@ -10,6 +10,12 @@ var sacha = {
   drone: true,
 };
 
+var Juan = {
+  nombre: 'Juan',
+  apellidos: 'Gomez',
+  edad: 13,
+};
+
 function imprimirProfesiones(persona) {
   console.log(`${persona.nombre} es:`);
   if (persona.ingeniero) {
@@ -36,8 +42,14 @@ function imprimirProfesiones(persona) {
 
 imprimirProfesiones(sacha);
 
+const MAYORIA_DE_EDAD = 18;
+
+function esMayorDeEdad(persona) {
+  return persona.edad >= MAYORIA_DE_EDAD;
+}
+
 function imprimirSiEsMayorDeEdad(persona) {
-  if (persona.edad >= 18) {
+  if (esMayorDeEdad(persona)) {
     console.log(`${persona.nombre} es mayor de edad`);
   } else {
     console.log(`${persona.nombre} es menor de edad`);
@@ -45,3 +57,4 @@ function imprimirSiEsMayorDeEdad(persona) {
 }
 
 imprimirSiEsMayorDeEdad(sacha);
+imprimirSiEsMayorDeEdad(Juan);
